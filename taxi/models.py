e3445538b6ab9ec43af26d664838d55067e682d1
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-01-05 11:58:09 +0100

    solution3

--- taxi/models.py
+++ taxi/models.py
@@ -17,6 +17,7 @@ class Manufacturer(models.Model):
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=100, unique=True)
 
+
 class Car(models.Model):
     model = models.CharField(max_length=100)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
